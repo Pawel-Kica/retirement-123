720469d8985b52c1ce7ffc19fe5a92a5fe90d619
Pawel-Kica <pawel.kica.cc@proton.me>
2025-10-04 20:34:14 +0200

    Remove HistorySidebar component from RootLayout to streamline the application layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
         <SimulationProvider>
           <Header />
           {children}
-          <HistorySidebar />
         </SimulationProvider>
       </ClientBody>
     </html>
